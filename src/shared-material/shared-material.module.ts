282305719918b008b1a2ac354bdf2d291a680030
jameskkk <chenjhkk@gmail.com>
2021-08-03 10:44:20 +0800

    Commit day 2 sample.

---
+++ src/shared-material/shared-material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  exports: [
+    MatButtonModule
+  ]
+})
+export class SharedMaterialModule {}
